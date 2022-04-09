cd6fd7cbfcd04adb367843024cd49010b1ed6638
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-04-09 12:18:08 +0100

    added button to Header, added collection details section

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,37 +12,25 @@ export default function Index() {
         <div className="flex items-center w-full space-x-3">
           <h1 className="text-2xl font-bold">Collection dApp</h1>
           <input type="text" placeholder="Search by contract address..." className="pl-10 pr-3 max-w-sm w-full ring-1 ring-[#ed194a]" />
+          <button className="p-2 rounded-lg bg-gradient-to-br from-[#dd0baf] to-[#8602bc]">Search Collection</button>
         </div>
         <div className="themeToggle">
           <span>ICON</span>
         </div>
       </div>
-      <h1 className="text-3xl font-bold text-red-500">Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+     <div className="flex border border-red-400">
+       <div className="w-full border details">
+          <div className="flex flex-col items-center justify-center h-full">
+            <img src="" alt="collection image" className="border rounded-full w-28 h-28" />
+            <h2>Collection Name</h2>
+            <div className="space-x-4 socialIcons">
+              <span className="text-gray-600 hover:text-[#2081e2]">OpenSea</span>
+              <span className="text-gray-600 duration-300 hover:text-[#1DA1F2]">Twitter</span>
+              <span className="text-gray-600 hover:">Website</span>
+            </div>
+            </div>
+       </div>
+     </div>
     </div>
   );
 }
